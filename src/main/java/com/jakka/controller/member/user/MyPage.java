0f7a2e47947a419da652edec24f57bfcd8eba7f4
sol2ee <155609506+sol2ee@users.noreply.github.com>
2024-05-07 17:50:06 +0900

    마이페이지 수정중 ..

--- src/main/java/com/jakka/controller/member/user/MyPage.java
+++ src/main/java/com/jakka/controller/member/user/MyPage.java
@@ -37,10 +37,42 @@ public class MyPage extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/user/mypage.jsp");
-	    dispatcher.forward(req, resp);
-	}
+		    req.setCharacterEncoding("UTF-8");
+		    
+		    HttpSession session = req.getSession();
+		    String userId = (String) session.getAttribute("userId");
+
+		    // 사용자가 입력한 정보 가져오기
+		    String nickName = req.getParameter("nickName");
+		    String phoneStart = req.getParameter("phoneStart");
+		    String phoneInput1 = req.getParameter("phoneInput1");
+		    String phoneInput2 = req.getParameter("phoneInput2");
+		    String address = req.getParameter("address");
+		    String email = req.getParameter("email");
+		    
+		    UserDTO dto = new UserDTO();
+		    
+		    dto.setUserId(userId);
+		    dto.setUserNick(nickName);
+		    dto.setUserTel(phoneStart + "-" + phoneInput1 + "-" + phoneInput2);
+		    dto.setUserAddress(address);
+		    dto.setUserEmail(email);
+		    
+		    // DAO를 통해 정보 업데이트
+		    UserDAO dao = DAOManager.getUserDAO();
+		    int result = dao.save(dto);
+
+		    if (result > 0) {
+		        // 업데이트 성공
+		        // 마이페이지로 이동
+		        resp.sendRedirect(req.getContextPath() + "/user/mypage.do");
+		    } else {
+		        // 업데이트 실패
+		        resp.sendRedirect(req.getContextPath() + "/error.jsp");
+		    }
+		}
+
+
 
 	
 }//End of class
